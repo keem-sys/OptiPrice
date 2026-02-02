a589c634cf64d50a0cde8d7a6229c2fe079b14b1
Ebenezer <ebenkouakou7@gmail.com>
2026-02-02 17:48:06 +0200

    Initial scaffolding

---
+++ src/main/java/com/optiprice/OptipriceApplication.java
@@ -0,0 +1,13 @@
+package com.optiprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptipriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptipriceApplication.class, args);
+	}
+
+}
