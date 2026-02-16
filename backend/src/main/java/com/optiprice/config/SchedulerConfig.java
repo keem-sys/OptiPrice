85543e51fd430122ef2d520a0bc9374b2939ff39
Ebenezer <ebenkouakou7@gmail.com>
2026-02-16 12:59:59 +0200

    add scheduler config

--- backend/src/main/java/com/optiprice/config/SchedulerConfig.java
+++ backend/src/main/java/com/optiprice/config/SchedulerConfig.java
@@ -1,4 +1,16 @@
 package com.optiprice.config;
 
-public class SchedulerConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(0,
+                Thread.ofVirtual().name("opti-scheduler-", 0).factory()));
+    }
 }
